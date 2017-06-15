1df06584553d411c432581a8412fadb7d0efdd7a
Brandon Crews <bdc1978@gmail.com>
2017-06-15 10:27:07 -0700

    Added endpoint for /recipes

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const router = express.Router();
 const morgan = require('morgan');
 const bodyParser = require('body-parser');
 
-const {ShoppingList} = require('./models');
+const {ShoppingList, Recipes} = require('./models');
 
 const jsonParser = bodyParser.json();
 const app = express();
@@ -12,16 +12,20 @@ const app = express();
 // log the http layer
 app.use(morgan('common'));
 
-// we're going to add some items to ShoppingList
-// so there's some data to look at
 ShoppingList.create('beans', 2);
 ShoppingList.create('tomatoes', 3);
 ShoppingList.create('peppers', 4);
 
-// when the root of this router is called with GET, return
-// all current ShoppingList items
+Recipes.create('chocolate milk', ['cocoa', 'milk', 'sugar']);
+Recipes.create('lemonaid', ['lemon', 'water', 'sugar']);
+Recipes.create('cheesybread', ['bread', 'cheese']);
+
 app.get('/shopping-list', (req, res) => {
   res.json(ShoppingList.get());
+});
+
+app.get('/recipes', (req, res) => {
+  res.json(Recipes.get());
 });
 
 app.listen(process.env.PORT || 8080, () => {
